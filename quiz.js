602fc4049b1971544d664d8ceca97fca9bb8b916
mrwork345 <mrwork345@gmail.com>
2025-10-18 15:00:29 +0500

    Refactor quiz.js and add score tracking functions

--- quiz.js
+++ quiz.js
@@ -5,3 +5,22 @@ function as() {
 function ab() {
     document.getElementById("a").innerHTML="WRONG"
 }
+function music() {
+    document.getElementById("c").innerHTML="always Music"
+    answeredQuestions++;
+    checkScore();
+    score++;
+}
+function games() {
+    document.getElementById("c").innerHTML="Bad choice baby"
+    answeredQuestions++;
+    checkScore();
+}
+function checkScore() {
+    if (answeredQuestions === 3) {
+        document.getElementById("score").innerHTML = "🎯 Your Total Score: " + score + " / 3";
+    }
+}
+
+
+
