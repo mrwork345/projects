f32069e7b4a3ce01c2bf46dff5e022b53558c806
mrwork345 <mrwork345@gmail.com>
2025-10-27 16:04:14 +0500

    Refactor quiz functions to include scoring logic

--- quiz.js
+++ quiz.js
@@ -1,9 +1,32 @@
+let score = 0;
+let answeredQuestions = 0;
+
 
 function as() {
     document.getElementById("a").innerHTML = "CORRECT 🏆";
+    score++;
+    answeredQuestions++;
+    checkScore();
 }
+
 function ab() {
-    document.getElementById("a").innerHTML="WRONG"
+    document.getElementById("a").innerHTML = "WRONG ❌";
+    answeredQuestions++;
+    checkScore();
+}
+
+
+function im() {
+    document.getElementById("b").innerHTML = "WRONG ❌";
+    answeredQuestions++;
+    checkScore();
+}
+
+function mr() {
+    document.getElementById("b").innerHTML = "CORRECT 🏆";
+    score++;
+    answeredQuestions++;
+    checkScore();
 }
 function music() {
     document.getElementById("c").innerHTML="always Music"
@@ -21,6 +44,11 @@ function checkScore() {
         document.getElementById("score").innerHTML = "🎯 Your Total Score: " + score + " / 3";
     }
 }
+
+
+
+
+
 
 
 
